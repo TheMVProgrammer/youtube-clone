f4647e2be44de8db73c04666a540573bd4127477
TheMVProgrammer <franklin0415@hotmail.com>
2023-01-09 16:25:10 -0400

    Finishing Navbar and starting Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const Sidebar = () => (
+    <Stack
+        direction="row"
+        sx={{
+            overflowY: "auto",
+            height: { sx: 'auto', md: '92vh' }
+        }}
+    >
+
+    </Stack>
+)
+
+export default Sidebar
